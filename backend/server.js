ff91ceb95020df2ce745d163bdf61515b9aff5ff
flyingwood34 <surajyadav34981@gmail.com>
2026-03-13 14:18:44 +0530

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,20 +4,27 @@ const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
-// ONLY expense routes
+// Root route
+app.get("/", (req, res) => {
+  res.send("Smart Expense Analyzer API Running");
+});
+
+// API routes
 app.use("/api/expenses", require("./routes/expenseRoutes"));
 app.use("/api/categories", require("./routes/categoryRoutes"));
 
+const PORT = process.env.PORT || 5000;
 
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB connected");
-    app.listen(5000, () =>
-      console.log("Server running on port 5000")
+    app.listen(PORT, () =>
+      console.log(`Server running on port ${PORT}`)
     );
   })
   .catch((err) => console.log(err));
