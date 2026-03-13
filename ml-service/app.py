710edf396cb44f599bc82ed75b16d24bbe9aa962
flyingwood34 <surajyadav34981@gmail.com>
2026-03-13 15:25:47 +0530

    update ml port

--- ml-service/app.py
+++ ml-service/app.py
@@ -1,7 +1,12 @@
 from flask import Flask, request, jsonify
 from model import predict_next_month
+import os
 
 app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "ML Service Running"
 
 @app.route("/predict", methods=["POST"])
 def predict():
@@ -10,4 +15,5 @@ def predict():
     return jsonify({"prediction": prediction})
 
 if __name__ == "__main__":
-    app.run(port=5001)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
